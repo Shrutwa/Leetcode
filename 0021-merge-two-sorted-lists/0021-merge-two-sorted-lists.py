64e9863762d0284407b2d7dbbd161cea218a8082
Shrutwa <43299507+Shrutwa@users.noreply.github.com>
2022-11-19 17:58:24 -0700

    Time: 40 ms (64.25%), Space: 13.5 MB (32.06%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -0,0 +1,58 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    
+    def list_to_listnode(self, pylist, link_count):
+        if len(pylist) > 1:
+            ret = precompiled.listnode.ListNode(pylist.pop())
+            ret.next = self.list_to_listnode(pylist, link_count)
+            return ret
+        elif len(pylist)>0:
+            return precompiled.listnode.ListNode(pylist.pop(), None)
+        else:
+            return ListNode('', None)
+        
+    def listnode_to_list(self, listnode):
+        res_list = []
+        
+        if listnode == None:
+            return res_list
+        else: 
+            while True:
+                res_list.append(listnode.val)
+                if listnode.next != None:
+                    listnode = listnode.next
+                else:
+                    return res_list
+    
+            
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        print(list1)
+        print(list2)
+            
+        l1 = []
+        l2 = []
+        sorted_list = []
+        
+        
+        l1 = self.listnode_to_list(list1)
+        l2 = self.listnode_to_list(list2)
+        
+        # print(type(l1))
+        # print(type(l2))
+        
+        
+        sorted_list = sorted(l1 + l2, reverse=True)
+        ret_list = self.list_to_listnode(sorted_list,len(sorted_list))
+        
+        return ret_list
+        
+        
